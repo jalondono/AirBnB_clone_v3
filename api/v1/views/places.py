aad8905381a0a52a4333db2b23d11a2cb3afe5e8
Alafresh <868@holbertonschool.com>
2020-01-30 19:51:41 -0500

    Update the function create_place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -56,27 +56,24 @@ def delete_place(place_id):
 def create_place(city_id):
     """This function creates a new place"""
     data = request.get_json()
-    cities = storage.all(City)
-    c_id = "City." + city_id
-    if cities.get(c_id) is None:
+    c_id = storage.get("City", city_id)
+    if c_id is None:
         abort(404)
     if not data:
         abort(400, 'Not a JSON')
-    if 'user_id' not in data:
+    if data.get('user_id') is None:
         abort(400, 'Missing user_id')
     u_id = data['user_id']
     look_user = storage.get("User", u_id)
     if look_user is None:
         abort(404)
-    if 'name' not in data:
+    if data.get('name') is None:
         abort(400, 'Missing name')
-    place = Place()
-    place.city_id = data['city_id']
-    place.email = data['name']
-    place.user_id = data['user_id']
-    place.save()
-    place = place.to_dict()
-    return jsonify(place), 201
+    data['city_id'] = city_id
+    place = Place(**data)
+    storage.new(place)
+    storage.save()
+    return jsonify(place.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
